f68d4b514f3bcafc4debe42799630aa16aa72200
masterblacksmith <82542983+masterblacksmith@users.noreply.github.com>
2021-05-30 02:39:55 -0700

    feature(staking): adding quests

--- src/constants/staking.ts
+++ src/constants/staking.ts
@@ -7,7 +7,28 @@ export const STAKING_REWARDS_INFO: {
     tokens: [Token, Token]
   }[]
 } = {
-  [ChainId.HARMONY_MAINNET]: [],
+  [ChainId.HARMONY_MAINNET]: [
+    {
+      pid: 0,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBNB/WONE')
+    },
+    {
+      pid: 1,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBUSD/WONE')
+    },
+    {
+      pid: 2,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1ETH/WONE')
+    },
+    {
+      pid: 4,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WBTC/WONE')
+    },
+    {
+      pid: 5,
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LOOT/WONE')
+    }
+  ],
   [ChainId.HARMONY_TESTNET]: [
     {
       pid: 0,
