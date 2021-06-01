siddarthsh/venomswap-interface

Add support for archived pools

src/constants/staking.ts
@@ -1,124 +1,154 @@
 import { ChainId, Token } from '@venomswap/sdk'
 import getPairTokensWithDefaults from '../utils/getPairTokensWithDefaults'
 
+export interface StakingRewardsInfo {
+  pid: number
+  tokens: [Token, Token]
+  active: boolean
+}
+
 export const STAKING_REWARDS_INFO: {
-  [chainId in ChainId]?: {
-    pid: number
-    tokens: [Token, Token]
-  }[]
+  [chainId in ChainId]?: StakingRewardsInfo[]
 } = {
   [ChainId.HARMONY_MAINNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/BUSD')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/BUSD'),
+      active: true
     },
     {
       pid: 1,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/VIPER'),
+      active: true
     },
     {
       pid: 2,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1ETH')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1ETH'),
+      active: true
     },
     {
       pid: 3,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/VIPER'),
+      active: true
     },
     {
       pid: 4,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/bscBUSD')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'BUSD/bscBUSD'),
+      active: true
     },
     {
       pid: 5,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1USDC')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1USDC'),
+      active: true
     },
     {
       pid: 6,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1ROT/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1ROT/VIPER'),
+      active: true
     },
     {
       pid: 7,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1MAGGOT/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1MAGGOT/VIPER'),
+      active: true
     },
     {
       pid: 8,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WISE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WISE/VIPER'),
+      active: true
     },
     {
       pid: 9,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1DSLA/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1DSLA/VIPER'),
+      active: true
     },
     {
       pid: 10,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LINK/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LINK/VIPER'),
+      active: true
     },
     {
       pid: 11,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1AAVE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1AAVE/VIPER'),
+      active: true
     },
     {
       pid: 12,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1SNX/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1SNX/VIPER'),
+      active: true
     },
     {
       pid: 13,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1YFI/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1YFI/VIPER'),
+      active: true
     },
     {
       pid: 14,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '11INCH/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '11INCH/VIPER'),
+      active: true
     },
     {
       pid: 15,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscCAKE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscCAKE/VIPER'),
+      active: true
     },
     {
       pid: 16,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1SUSHI/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1SUSHI/VIPER'),
+      active: true
     },
     {
       pid: 17,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1UNI/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1UNI/VIPER'),
+      active: true
     },
     {
       pid: 18,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WISE/1ETH')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WISE/1ETH'),
+      active: true
     },
     {
       pid: 19,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1WBTC')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1WBTC'),
+      active: true
     },
     {
       pid: 20,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1MATIC')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'WONE/1MATIC'),
+      active: true
     }
   ],
   [ChainId.HARMONY_TESTNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/1BUSD')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/1BUSD'),
+      active: true
     },
     {
       pid: 1,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/VIPER'),
+      active: true
     },
     {
       pid: 2,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/1ETH')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, 'WONE/1ETH'),
+      active: true
     },
     {
       pid: 3,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, '1BUSD/VIPER')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_TESTNET, '1BUSD/VIPER'),
+      active: true
     }
   ],
   [ChainId.BSC_TESTNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/BUSD')
+      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/BUSD'),
+      active: true
     },
     {
       pid: 1,
-      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/COBRA')
+      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/COBRA'),
+      active: true
     }
   ]
 }

src/hooks/useCalculateStakingInfoMembers.ts
@@ -1,0 +1,21 @@
+import { ChainId } from '@venomswap/sdk'
+import { useMemo } from 'react'
+import { STAKING_REWARDS_INFO } from '../constants/staking'
+
+export default function useCalculateStakingInfoMembers(
+  chainId: ChainId | undefined
+): Record<string, number | undefined> {
+  return useMemo(() => {
+    const activeStakingInfos = chainId
+      ? STAKING_REWARDS_INFO[chainId as ChainId]?.filter(stakingRewardInfo => stakingRewardInfo.active)
+      : []
+    const inactiveStakingInfos = chainId
+      ? STAKING_REWARDS_INFO[chainId as ChainId]?.filter(stakingRewardInfo => !stakingRewardInfo.active)
+      : []
+
+    return {
+      active: activeStakingInfos?.length,
+      inactive: inactiveStakingInfos?.length
+    }
+  }, [chainId])
+}

src/hooks/useFilterStakingInfo.ts
@@ -1,0 +1,28 @@
+import { ChainId, Pair } from '@venomswap/sdk'
+import { useMemo } from 'react'
+import { STAKING_REWARDS_INFO, StakingRewardsInfo } from '../constants/staking'
+
+export default function useFilterStakingInfo(
+  chainId: ChainId | undefined,
+  active: boolean | undefined = undefined,
+  pairToFilterBy?: Pair | null
+): StakingRewardsInfo[] {
+  return useMemo(() => {
+    const pools = chainId
+      ? STAKING_REWARDS_INFO[chainId]?.filter(stakingRewardInfo =>
+          pairToFilterBy === undefined
+            ? true
+            : pairToFilterBy === null
+            ? false
+            : pairToFilterBy.involvesToken(stakingRewardInfo.tokens[0]) &&
+              pairToFilterBy.involvesToken(stakingRewardInfo.tokens[1])
+        ) ?? []
+      : []
+
+    if (active !== undefined) {
+      return pools?.filter(stakingRewardInfo => stakingRewardInfo.active === active) ?? []
+    }
+
+    return pools
+  }, [chainId, active, pairToFilterBy])
+}

masterblacksmith/lootswap-interface

src/constants/staking.ts
@@ -1,39 +1,48 @@
 import { ChainId, Token } from '@lootswap/sdk'
 import getPairTokensWithDefaults from '../utils/getPairTokensWithDefaults'
 
+export interface StakingRewardsInfo {
+  pid: number
+  tokens: [Token, Token]
+  active: boolean
+}
+
 export const STAKING_REWARDS_INFO: {
-  [chainId in ChainId]?: {
-    pid: number
-    tokens: [Token, Token]
-  }[]
+  [chainId in ChainId]?: StakingRewardsInfo[]
 } = {
   [ChainId.HARMONY_MAINNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBNB/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBNB/WONE'),
+      active: true
     },
     {
       pid: 1,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBUSD/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'bscBUSD/WONE'),
+      active: true
     },
     {
       pid: 2,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1ETH/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1ETH/WONE'),
+      active: true
     },
     {
       pid: 4,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WBTC/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, '1WBTC/WONE'),
+      active: true
     },
     {
       pid: 5,
-      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LOOT/WONE')
+      tokens: getPairTokensWithDefaults(ChainId.HARMONY_MAINNET, 'LOOT/WONE'),
+      active: true
     }
   ],
   [ChainId.HARMONY_TESTNET]: [],
   [ChainId.BSC_TESTNET]: [
     {
       pid: 0,
-      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/BUSD')
+      tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'WBNB/BUSD'),
+      active: true
     }
   ]
 }

src/hooks/useCalculateStakingInfoMembers.ts
@@ -1,0 +1,21 @@
+import { ChainId } from '@lootswap/sdk'
+import { useMemo } from 'react'
+import { STAKING_REWARDS_INFO } from '../constants/staking'
+
+export default function useCalculateStakingInfoMembers(
+  chainId: ChainId | undefined
+): Record<string, number | undefined> {
+  return useMemo(() => {
+    const activeStakingInfos = chainId
+      ? STAKING_REWARDS_INFO[chainId as ChainId]?.filter(stakingRewardInfo => stakingRewardInfo.active)
+      : []
+    const inactiveStakingInfos = chainId
+      ? STAKING_REWARDS_INFO[chainId as ChainId]?.filter(stakingRewardInfo => !stakingRewardInfo.active)
+      : []
+
+    return {
+      active: activeStakingInfos?.length,
+      inactive: inactiveStakingInfos?.length
+    }
+  }, [chainId])
+}

src/hooks/useFilterStakingInfo.ts
@@ -1,0 +1,28 @@
+import { ChainId, Pair } from '@lootswap/sdk'
+import { useMemo } from 'react'
+import { STAKING_REWARDS_INFO, StakingRewardsInfo } from '../constants/staking'
+
+export default function useFilterStakingInfo(
+  chainId: ChainId | undefined,
+  active: boolean | undefined = undefined,
+  pairToFilterBy?: Pair | null
+): StakingRewardsInfo[] {
+  return useMemo(() => {
+    const pools = chainId
+      ? STAKING_REWARDS_INFO[chainId]?.filter(stakingRewardInfo =>
+          pairToFilterBy === undefined
+            ? true
+            : pairToFilterBy === null
+            ? false
+            : pairToFilterBy.involvesToken(stakingRewardInfo.tokens[0]) &&
+              pairToFilterBy.involvesToken(stakingRewardInfo.tokens[1])
+        ) ?? []
+      : []
+
+    if (active !== undefined) {
+      return pools?.filter(stakingRewardInfo => stakingRewardInfo.active === active) ?? []
+    }
+
+    return pools
+  }, [chainId, active, pairToFilterBy])
+}