siddarthsh/venomswap-interface

Display staked balance in the pit

src/pages/Pit/index.tsx
@@ -130,6 +130,7 @@ export default function Pit({
   const pitSettings = chainId ? PIT_SETTINGS[chainId] : undefined
   const pitBalance: TokenAmount | undefined = useTokenBalance(account ?? undefined, pit, 'balanceOf', PIT_INTERFACE)
   const govTokenPitTokenRatio = usePitRatio()
+  const adjustedPitBalance = govTokenPitTokenRatio ? pitBalance?.multiply(govTokenPitTokenRatio) : undefined
 
   const userLiquidityStaked = pitBalance
   const userLiquidityUnstaked = govTokenBalance
@@ -238,10 +239,17 @@ export default function Pit({
           </StyledBottomCard>
         </BottomSection>
 
-        {account && (
+        {account && adjustedPitBalance && adjustedPitBalance?.greaterThan('0') && (
+          <TYPE.main>
+            You have {adjustedPitBalance?.toFixed(2, { groupSeparator: ',' })} {govToken?.symbol} tokens staked in
+            the&nbsp;{pitSettings?.name}.
+          </TYPE.main>
+        )}
+
+        {account && (!adjustedPitBalance || adjustedPitBalance?.equalTo('0')) && (
           <TYPE.main>
             You have {govTokenBalance?.toFixed(2, { groupSeparator: ',' })} {govToken?.symbol} tokens available to
-            deposit to the {pitSettings?.name}
+            deposit to the {pitSettings?.name}.
           </TYPE.main>
         )}
 

masterblacksmith/lootswap-interface

src/pages/Dungeon/index.tsx
@@ -135,6 +135,9 @@ export default function Dungeon({
     DUNGEON_INTERFACE
   )
   const govTokenDungeonTokenRatio = useDungeonRatio()
+  const adjustedDungeonBalance = govTokenDungeonTokenRatio
+    ? dungeonBalance?.multiply(govTokenDungeonTokenRatio)
+    : undefined
 
   const userLiquidityStaked = dungeonBalance
   const userLiquidityUnstaked = govTokenBalance
@@ -243,10 +246,17 @@ export default function Dungeon({
           </StyledBottomCard>
         </BottomSection>
 
-        {account && (
+        {account && adjustedDungeonBalance && adjustedDungeonBalance?.greaterThan('0') && (
+          <TYPE.main>
+            You have {adjustedDungeonBalance?.toFixed(2, { groupSeparator: ',' })} {govToken?.symbol} tokens staked in
+            the&nbsp;{dungeonSettings?.name}.
+          </TYPE.main>
+        )}
+
+        {account && (!adjustedDungeonBalance || adjustedDungeonBalance?.equalTo('0')) && (
           <TYPE.main>
             You have {govTokenBalance?.toFixed(2, { groupSeparator: ',' })} {govToken?.symbol} tokens available to
-            deposit to the {dungeonSettings?.name}
+            deposit to the {dungeonSettings?.name}.
           </TYPE.main>
         )}
 