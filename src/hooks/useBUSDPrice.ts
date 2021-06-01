siddarthsh/venomswap-interface

Remove deposit fee, testnet fixes

src/hooks/useBUSDPrice.ts
@@ -1,4 +1,4 @@
-import { Currency, currencyEquals, JSBI, Token, Price, WETH } from '@venomswap/sdk'
+import { Currency, currencyEquals, JSBI, Token, Price, WETH, ChainId } from '@venomswap/sdk'
 import { useMemo } from 'react'
 import { PairState, usePairs } from '../data/Reserves'
 import { useActiveWeb3React } from '.'
@@ -12,7 +12,8 @@ import getToken from '../utils/getToken'
 export default function useBUSDPrice(currency?: Currency): Price | undefined {
   const { chainId } = useActiveWeb3React()
   const wrapped = wrappedCurrency(currency, chainId)
-  const busd: Token | undefined = getToken(chainId, 'BUSD')
+  const busdTicker = chainId !== ChainId.HARMONY_TESTNET ? 'BUSD' : '1BUSD'
+  const busd: Token | undefined = getToken(chainId, busdTicker)
 
   const tokenPairs: [Currency | undefined, Currency | undefined][] = useMemo(
     () => [

src/hooks/useTokensWithWETHPrices.ts
@@ -1,4 +1,4 @@
-import { WETH, Token, Blockchain } from '@venomswap/sdk'
+import { ChainId, WETH, Token, Blockchain } from '@venomswap/sdk'
 import { useMemo } from 'react'
 import useGovernanceToken from './useGovernanceToken'
 import useTokenWethPrice from './useTokenWethPrice'
@@ -15,7 +15,8 @@ export default function useTokensWithWethPrices(): Record<string, any> {
   const govToken = useGovernanceToken()
   const govTokenWETHPrice = useTokenWethPrice(govToken)
 
-  const BUSD: Token | undefined = getToken(chainId, 'BUSD')
+  const BUSDTicker = chainId !== ChainId.HARMONY_TESTNET ? 'BUSD' : '1BUSD'
+  const BUSD: Token | undefined = getToken(chainId, BUSDTicker)
   const BUSDWETHPrice = useTokenWethPrice(BUSD)
 
   const USDCTicker = blockchain === Blockchain.HARMONY ? '1USDC' : 'USDC'

masterblacksmith/lootswap-interface

src/hooks/useBUSDPrice.ts
@@ -1,4 +1,4 @@
-import { Currency, currencyEquals, JSBI, Token, Price, WETH } from '@lootswap/sdk'
+import { Currency, currencyEquals, JSBI, Token, Price, WETH, ChainId } from '@lootswap/sdk'
 import { useMemo } from 'react'
 import { PairState, usePairs } from '../data/Reserves'
 import { useActiveWeb3React } from '.'
@@ -12,7 +12,8 @@ import getToken from '../utils/getToken'
 export default function useBUSDPrice(currency?: Currency): Price | undefined {
   const { chainId } = useActiveWeb3React()
   const wrapped = wrappedCurrency(currency, chainId)
-  const busd: Token | undefined = getToken(chainId, 'BUSD')
+  const busdTicker = chainId !== ChainId.HARMONY_TESTNET ? 'BUSD' : '1BUSD'
+  const busd: Token | undefined = getToken(chainId, busdTicker)
 
   const tokenPairs: [Currency | undefined, Currency | undefined][] = useMemo(
     () => [

src/hooks/useTokensWithWETHPrices.ts
@@ -1,4 +1,4 @@
-import { WETH, Token, Blockchain } from '@lootswap/sdk'
+import { ChainId, WETH, Token, Blockchain } from '@lootswap/sdk'
 import { useMemo } from 'react'
 import useGovernanceToken from './useGovernanceToken'
 import useTokenWETHPrice from './useTokenWETHPrice'
@@ -15,7 +15,8 @@ export default function useTokensWithWETHPrices(): Record<string, any> {
   const govToken = useGovernanceToken()
   const govTokenWETHPrice = useTokenWETHPrice(govToken)
 
-  const BUSD: Token | undefined = getToken(chainId, 'BUSD')
+  const BUSDTicker = chainId !== ChainId.HARMONY_TESTNET ? 'BUSD' : '1BUSD'
+  const BUSD: Token | undefined = getToken(chainId, BUSDTicker)
   const BUSDWETHPrice = useTokenWETHPrice(BUSD)
 
   const USDCTicker = blockchain === Blockchain.HARMONY ? '1USDC' : 'USDC'