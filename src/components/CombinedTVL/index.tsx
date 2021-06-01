siddarthsh/venomswap-interface

Fix TVL display

src/components/CombinedTVL/index.tsx
@@ -14,11 +14,11 @@ export default function CombinedTVL({}) {
 
   return (
     <>
-      {TVLs && (
+      {TVLs?.stakingPoolTVL?.greaterThan('0') && (
         <CustomMouseoverTooltip
           element={
             <>
-              {TVLs.stakingPoolTVL && (
+              {TVLs.stakingPoolTVL?.greaterThan('0') && (
                 <>
                   <b>Staking:</b> $
                   {TVLs.stakingPoolTVL.toSignificant(8, {
@@ -27,13 +27,13 @@ export default function CombinedTVL({}) {
                   <br />
                 </>
               )}
-              {TVLs.totalPitTVL && (
+              {TVLs.totalPitTVL?.greaterThan('0') && (
                 <>
                   <b>{pitSettings?.name}:</b> ${TVLs.totalPitTVL.toSignificant(8, { groupSeparator: ',' })}
                   <br />
                 </>
               )}
-              {TVLs.totalCombinedTVL && (
+              {TVLs.totalCombinedTVL?.greaterThan('0') && (
                 <>
                   <b>Total:</b> ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}
                 </>
@@ -41,7 +41,9 @@ export default function CombinedTVL({}) {
             </>
           }
         >
-          {TVLs.totalCombinedTVL && <>TVL: ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}</>}
+          {TVLs.totalCombinedTVL?.greaterThan('0') && (
+            <>TVL: ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}</>
+          )}
         </CustomMouseoverTooltip>
       )}
     </>

masterblacksmith/lootswap-interface

src/components/CombinedTVL/index.tsx
@@ -14,11 +14,11 @@ export default function CombinedTVL({}) {
 
   return (
     <>
-      {TVLs && (
+      {TVLs?.stakingPoolTVL?.greaterThan('0') && (
         <CustomMouseoverTooltip
           element={
             <>
-              {TVLs.stakingPoolTVL && (
+              {TVLs.stakingPoolTVL?.greaterThan('0') && (
                 <>
                   <b>Staking:</b> $
                   {TVLs.stakingPoolTVL.toSignificant(8, {
@@ -27,13 +27,13 @@ export default function CombinedTVL({}) {
                   <br />
                 </>
               )}
-              {TVLs.totalDungeonTVL && (
+              {TVLs.totalDungeonTVL?.greaterThan('0') && (
                 <>
                   <b>{dungeonSettings?.name}:</b> ${TVLs.totalDungeonTVL.toSignificant(8, { groupSeparator: ',' })}
                   <br />
                 </>
               )}
-              {TVLs.totalCombinedTVL && (
+              {TVLs.totalCombinedTVL?.greaterThan('0') && (
                 <>
                   <b>Total:</b> ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}
                 </>
@@ -41,7 +41,9 @@ export default function CombinedTVL({}) {
             </>
           }
         >
-          {TVLs.totalCombinedTVL && <>TVL: ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}</>}
+          {TVLs.totalCombinedTVL?.greaterThan('0') && (
+            <>TVL: ${TVLs.totalCombinedTVL.toSignificant(8, { groupSeparator: ',' })}</>
+          )}
         </CustomMouseoverTooltip>
       )}
     </>