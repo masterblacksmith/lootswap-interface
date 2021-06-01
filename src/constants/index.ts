siddarthsh/venomswap-interface

Display xGovToken -> GovToken ratio

src/constants/index.ts
@@ -209,11 +209,7 @@ export const SUGGESTED_BASES: ChainTokenList = {
   [ChainId.HARMONY_MAINNET]: [
     ...WETH_ONLY[ChainId.HARMONY_MAINNET],
     getTokenWithDefault(ChainId.HARMONY_MAINNET, 'BUSD'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'bscBUSD'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, '1USDC'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'VIPER'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, '1ETH'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'LINK')
+    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'VIPER')
   ]
 }
 

src/pages/Pit/index.tsx
@@ -26,6 +26,7 @@ import { GOVERNANCE_TOKEN_INTERFACE } from '../../constants/abis/governanceToken
 import { PIT_INTERFACE } from '../../constants/abis/pit'
 import useGovernanceToken from 'hooks/useGovernanceToken'
 import useTotalCombinedTVL from '../../hooks/useTotalCombinedTVL'
+import usePitRatio from '../../hooks/usePitRatio'
 import { useStakingInfo } from '../../state/stake/hooks'
 import filterStakingInfos from '../../utils/filterStakingInfos'
 import CombinedTVL from '../../components/CombinedTVL'
@@ -128,6 +129,7 @@ export default function Pit({
   const pit = chainId ? PIT[chainId] : undefined
   const pitSettings = chainId ? PIT_SETTINGS[chainId] : undefined
   const pitBalance: TokenAmount | undefined = useTokenBalance(account ?? undefined, pit, 'balanceOf', PIT_INTERFACE)
+  const govTokenPitTokenRatio = usePitRatio()
 
   const userLiquidityStaked = pitBalance
   const userLiquidityUnstaked = govTokenBalance
@@ -209,7 +211,14 @@ export default function Pit({
             <AutoColumn gap="sm">
               <RowBetween>
                 <div>
-                  <TYPE.black>Your x{govToken?.symbol} Balance</TYPE.black>
+                  <TYPE.black>
+                    Your x{govToken?.symbol} Balance
+                    {govTokenPitTokenRatio && (
+                      <TYPE.italic display="inline" marginLeft="0.25em">
+                        (1 x{govToken?.symbol} = {govTokenPitTokenRatio.toSignificant(4)} {govToken?.symbol})
+                      </TYPE.italic>
+                    )}
+                  </TYPE.black>
                 </div>
               </RowBetween>
               <RowBetween style={{ alignItems: 'baseline' }}>

masterblacksmith/lootswap-interface

src/constants/index.ts
@@ -192,11 +192,7 @@ export const SUGGESTED_BASES: ChainTokenList = {
   [ChainId.HARMONY_MAINNET]: [
     ...WETH_ONLY[ChainId.HARMONY_MAINNET],
     getTokenWithDefault(ChainId.HARMONY_MAINNET, 'BUSD'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'bscBUSD'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, '1USDC'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'LOOT'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, '1ETH'),
-    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'LINK')
+    getTokenWithDefault(ChainId.HARMONY_MAINNET, 'LOOT')
   ]
 }
 

src/pages/Dungeon/index.tsx
@@ -26,6 +26,7 @@ import { GOVERNANCE_TOKEN_INTERFACE } from '../../constants/abis/governanceToken
 import { DUNGEON_INTERFACE } from '../../constants/abis/dungeon'
 import useGovernanceToken from 'hooks/useGovernanceToken'
 import useTotalCombinedTVL from '../../hooks/useTotalCombinedTVL'
+import useDungeonRatio from '../../hooks/useDungeonRatio'
 import { useStakingInfo } from '../../state/stake/hooks'
 import filterStakingInfos from '../../utils/filterStakingInfos'
 import CombinedTVL from '../../components/CombinedTVL'
@@ -133,6 +134,7 @@ export default function Dungeon({
     'balanceOf',
     DUNGEON_INTERFACE
   )
+  const govTokenDungeonTokenRatio = useDungeonRatio()
 
   const userLiquidityStaked = dungeonBalance
   const userLiquidityUnstaked = govTokenBalance
@@ -214,7 +216,14 @@ export default function Dungeon({
             <AutoColumn gap="sm">
               <RowBetween>
                 <div>
-                  <TYPE.black>Your a{govToken?.symbol} Balance</TYPE.black>
+                  <TYPE.black>
+                    Your a{govToken?.symbol} Balance
+                    {govTokenDungeonTokenRatio && (
+                      <TYPE.italic display="inline" marginLeft="0.25em">
+                        (1 a{govToken?.symbol} = {govTokenDungeonTokenRatio.toSignificant(4)} {govToken?.symbol})
+                      </TYPE.italic>
+                    )}
+                  </TYPE.black>
                 </div>
               </RowBetween>
               <RowBetween style={{ alignItems: 'baseline' }}>